a13a3381382d12d62006befd916e6ee41a06b353
Greg Pfeil <greg@technomadic.org>
2026-01-13 10:26:54 -0700

    Extract flake outputs to a separate file
    The outputs are the only part of the flake that are actually a Nix
    expression[^1]. This makes it easier to deal with that by pulling `outputs` into
    an actual Nix file.
    
    [^1]: Even that isn’t quite true – `outputs` isn’t an arbitrary function, but
    must syntactically have a single argument. That is, we can’t η-reduce `outputs`
    to get rid of the `inputs` argument.
    
    This also helps with reducing templating – because flake.nix isn’t a Nix
    expression and since Project Manager runs from the flake, flake.nix _must_ be
    part of the template. Extracting outputs makes it easier to see what can
    actually be removed, and reduces conflicts in flake.nix when the template is
    updated.

--- flake.nix
+++ flake.nix
@@ -20,86 +20,9 @@
     use-registries = false;
   };
 
-  outputs = {
-    flake-utils,
-    flaky,
-    nixpkgs,
-    self,
-    systems,
-  }: let
-    sys = flake-utils.lib.system;
-
-    supportedSystems = import systems;
-
-    localPackages = pkgs: import ./packages {inherit pkgs;};
-  in
-    {
-      ## These are also consumed by downstream projects, so it may include more
-      ## than is referenced in this flake.
-      schemas = flaky.schemas;
-
-      overlays = {
-        default = nixpkgs.lib.composeManyExtensions [
-          flaky.overlays.default
-          self.overlays.dependencies
-          self.overlays.local
-        ];
-
-        dependencies = import ./nix/dependencies.nix;
-
-        local = final: prev: let
-          localPkgs = localPackages final;
-        in {
-          flaky-management-scripts = localPkgs.management-scripts;
-        };
-      };
-
-      templates = import ./templates;
-
-      homeConfigurations =
-        builtins.listToAttrs
-        (builtins.map
-          (flaky.lib.homeConfigurations.example self
-            [
-              ({pkgs, ...}: {
-                home.packages = [pkgs.flaky-management-scripts];
-                nixpkgs.overlays = [self.overlays.default];
-              })
-            ])
-          supportedSystems);
-    }
-    // flake-utils.lib.eachSystem supportedSystems
-    (system: let
-      pkgs = nixpkgs.legacyPackages.${system}.appendOverlays [
-        flaky.overlays.default
-        self.overlays.dependencies
-      ];
-    in {
-      devShells =
-        {default = flaky.lib.devShells.default system self [] "";}
-        // self.projectConfigurations.${system}.devShells
-        // import ./devShells {
-          inherit pkgs sys;
-          inherit (nixpkgs) lib;
-        };
-
-      apps.sync-template = flake-utils.lib.mkApp {
-        drv = self.packages.${system}.management-scripts;
-        name = "sync-template";
-      };
-
-      packages =
-        {default = self.packages.${system}.management-scripts;}
-        // localPackages pkgs;
-
-      projectConfigurations = flaky.lib.projectConfigurations.nix {
-        inherit pkgs self;
-        modules = [flaky.projectModules.bash];
-      };
-
-      checks = self.projectConfigurations.${system}.checks;
-      formatter = self.projectConfigurations.${system}.formatter;
-    });
+  ## The flake isn’t a Nix expression, so it’s clearer to keep `outputs` (which
+  ## is) in a separate file.
+  outputs = inputs: import .config/flake/outputs.nix inputs;
 
   inputs = {
     ## Flaky should generally be the source of truth for its inputs.
--- templates/bash/flake.nix
+++ templates/bash/flake.nix
@@ -20,95 +20,9 @@
     use-registries = false;
   };
 
-  outputs = {
-    flake-utils,
-    flaky,
-    nixpkgs,
-    self,
-    systems,
-  }: let
-    pname = "{{project.name}}";
-
-    supportedSystems = import systems;
-  in
-    {
-      schemas = {
-        inherit
-          (flaky.schemas)
-          overlays
-          homeConfigurations
-          apps
-          packages
-          devShells
-          projectConfigurations
-          checks
-          formatter
-          ;
-      };
-
-      overlays.default = final: prev: {};
-
-      lib = {};
-
-      homeConfigurations =
-        builtins.listToAttrs
-        (builtins.map
-          (flaky.lib.homeConfigurations.example self [
-            ({pkgs, ...}: {home.packages = [pkgs.${pname}];})
-          ])
-          supportedSystems);
-    }
-    // flake-utils.lib.eachSystem supportedSystems (system: let
-      pkgs = nixpkgs.legacyPackages.${system}.appendOverlays [
-        flaky.overlays.default
-      ];
-
-      src = pkgs.lib.cleanSource ./.;
-    in {
-      apps = {};
-
-      packages = {
-        default = self.packages.${system}.${pname};
-
-        "${pname}" = pkgs.checkedDrv (pkgs.stdenv.mkDerivation {
-          inherit pname src;
-
-          version = "0.1.0";
-
-          meta = {
-            description = "{{project.summary}}";
-            longDescription = ''
-              {{project.description}}
-            '';
-          };
-
-          nativeBuildInputs = [pkgs.bats];
-
-          patchPhase = ''
-            runHook prePatch
-            patchShebangs .
-            runHook postPatch
-          '';
-
-          doCheck = true;
-
-          checkPhase = ''
-            bats --print-output-on-failure ./test/all-tests.bats
-          '';
-
-          doInstallCheck = true;
-        });
-      };
-
-      projectConfigurations =
-        flaky.lib.projectConfigurations.bash {inherit pkgs self;};
-
-      devShells =
-        self.projectConfigurations.${system}.devShells
-        // {default = flaky.lib.devShells.default system self [] "";};
-      checks = self.projectConfigurations.${system}.checks;
-      formatter = self.projectConfigurations.${system}.formatter;
-    });
+  ## The flake isn’t a Nix expression, so it’s clearer to keep `outputs` (which
+  ## is) in a separate file.
+  outputs = inputs: import .config/flake/outputs.nix inputs;
 
   inputs = {
     ## Flaky should generally be the source of truth for its inputs.
