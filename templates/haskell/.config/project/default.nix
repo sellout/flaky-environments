1a8f3adbfb3a48193a243aed73ee70e4b5e05001
Greg Pfeil <greg@technomadic.org>
2026-08-02 21:21:47 -0600

    Remove more references to Garnix
    These were left behind by the last PR.

--- templates/haskell/.config/project/default.nix
+++ templates/haskell/.config/project/default.nix
@@ -69,7 +69,6 @@
   services.github.settings.branches.main.protection.required_status_checks.contexts =
     lib.mkForce
     ([
-        "All Garnix checks"
         "check-bounds"
         "check-licenses"
       ]
